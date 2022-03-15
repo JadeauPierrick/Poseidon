e4401bfbf9bc44957b2f133309f619f083ff0297
Pierrick Jadeau <jadeau.pierrick@gmail.com>
2022-03-15 12:14:25 +0100

    Ajout du fichier README

--- src/main/java/com/springapp/poseidon/controllers/LoginController.java
+++ src/main/java/com/springapp/poseidon/controllers/LoginController.java
@@ -2,7 +2,7 @@ package com.springapp.poseidon.controllers;
 
 import com.springapp.poseidon.service.GetUserInfoService;
 import com.springapp.poseidon.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +10,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import java.security.Principal;
 
+@Slf4j
 @Controller
 @RequestMapping("app")
 public class LoginController {
@@ -32,6 +33,7 @@ public class LoginController {
 
     @GetMapping("secure/article-details")
     public ModelAndView getAllUserArticles() {
+        log.info("Get user articles");
         ModelAndView mav = new ModelAndView();
         mav.addObject("users", userService.getUsers());
         mav.setViewName("user/list");
@@ -40,6 +42,7 @@ public class LoginController {
 
     @GetMapping("error")
     public ModelAndView error(Principal user) {
+        log.info("Error 403, the request is not authorized");
         ModelAndView mav = new ModelAndView();
         String errorMessage= "You are not authorized for the requested data.";
         mav.addObject("errorMsg", errorMessage);
